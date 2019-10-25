01fc16f9a295ed68bd636774fd71c25d5e682370
konica.patait <@dish.com>
2019-10-25 09:44:09 -0600

    Spring Sample

---
+++ SpringBootDockerTemplate/README.md
@@ -0,0 +1,4 @@
+# SpringBootDockerTemplate
+A template to create a docker image for spring boot application.
+
+[TODO add further details to setup the project and use]
---
+++ SpringBootDockerTemplate/src/main/java/com/docker/template/SpringDockerTemplate.java
@@ -0,0 +1,13 @@
+package com.docker.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerTemplate {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDockerTemplate.class, args);
+	}
+
+}
